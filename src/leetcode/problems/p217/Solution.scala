2400fdd69323ef299b80c18b2873fc18c76c7006
felleet <felleet@gmail.com>
2022-09-01 20:37:57 +0300

    [+] 217. Contains Duplicate

--- src/leetcode/problems/p217/Solution.scala
+++ src/leetcode/problems/p217/Solution.scala
@@ -1,5 +1,14 @@
 package leetcode.problems.p217
 
 object Solution {
-
+  def containsDuplicate(nums: Array[Int]): Boolean = {
+    val set = new collection.mutable.HashSet[Int]()
+    nums.foreach(n => {
+      if (set.contains(n)) {
+        return true
+      }
+      set.add(n)
+    })
+    false
+  }
 }
