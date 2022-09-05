324cb6e29c1d033f31d502f0c15d6074f2a9cfe9
felleet <felleet@gmail.com>
2022-09-05 17:14:41 +0300

    [+] 448. Find All Numbers Disappeared in an Array. Ver 2

--- src/leetcode/problems/p448/Solution.scala
+++ src/leetcode/problems/p448/Solution.scala
@@ -2,6 +2,13 @@ package leetcode.problems.p448
 
 object Solution {
   def findDisappearedNumbers(nums: Array[Int]): List[Int] = {
-    (1 to nums.length).diff(nums).toList
+    //(1 to nums.length).diff(nums).toList
+    val dict: Map[Int, Int] = nums.foldLeft(Map.empty[Int, Int])((m, number) =>
+      m + (number -> 1)
+    )
+
+    (1 to nums.length).filter(number => {
+      !dict.contains(number)
+    }).toList
   }
 }
