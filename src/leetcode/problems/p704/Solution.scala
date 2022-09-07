c9177cb8f2540066978267578110bb01092b9e46
felleet <felleet@gmail.com>
2022-09-07 10:28:27 +0300

    [+] 704. Binary Search

---
+++ src/leetcode/problems/p704/Solution.scala
@@ -0,0 +1,19 @@
+package leetcode.problems.p704
+
+object Solution {
+  def search(nums: Array[Int], target: Int): Int = {
+    var low = 0
+    var high = nums.length-1
+    while (low <= high) {
+      val mid = low + ((high-low) / 2)
+      if (nums(mid) < target) {
+        low = mid + 1
+      } else if (nums(mid) > target) {
+        high = mid - 1
+      } else {
+        return mid
+      }
+    }
+    -1
+  }
+}
