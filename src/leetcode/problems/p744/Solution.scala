8a235a4019d81bfb58312179564df4a4a46ad18b
felleet <felleet@gmail.com>
2022-09-08 10:12:49 +0300

    [+] 744. Find Smallest Letter Greater Than Target v2 one liner

--- src/leetcode/problems/p744/Solution.scala
+++ src/leetcode/problems/p744/Solution.scala
@@ -2,12 +2,6 @@ package leetcode.problems.p744
 
 object Solution {
   def nextGreatestLetter(letters: Array[Char], target: Char): Char = {
-    var min = letters(0)
-    for (i <- letters.indices) {
-      if (letters(i) > target) {
-        min = if (letters(i) < min || min <= target) letters(i) else min
-      }
-    }
-    min
+    letters.find(_ > target).getOrElse(letters(0))
   }
 }
