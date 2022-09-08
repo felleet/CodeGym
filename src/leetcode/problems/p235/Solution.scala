d290ae5f4ae87daafb1a192579cecaa9be2474b5
felleet <felleet@gmail.com>
2022-09-08 17:31:36 +0300

    [+] 235. Lowest Common Ancestor of a Binary Search Tree. v2

--- src/leetcode/problems/p235/Solution.scala
+++ src/leetcode/problems/p235/Solution.scala
@@ -6,36 +6,12 @@ import scala.collection.mutable
 
 object Solution {
   def lowestCommonAncestor(root: TreeNode, p: TreeNode, q: TreeNode): TreeNode = {
-    val left = new collection.mutable.Stack[TreeNode]()
-    val right = new collection.mutable.Stack[TreeNode]()
-    search(root, p, left)
-    search(root, q, right)
-
-    while (left.length != right.length && left.top != right.top) {
-      if (left.length > right.length) {
-        left.pop()
-      } else {
-        right.pop()
-      }
+    if (root.value > p.value && root.value > q.value) {
+      lowestCommonAncestor(root.left, p, q)
+    } else if (root.value < p.value && root.value < q.value) {
+      lowestCommonAncestor(root.right, p, q)
+    } else {
+      root
     }
-    left.pop()
-  }
-
-  def search(node: TreeNode, target: TreeNode, path: collection.mutable.Stack[TreeNode]): Boolean = {
-    if (node == null) {
-      return false
-    }
-    path.push( node)
-    if (node.value == target.value) {
-      return true
-    }
-    if (search(node.left, target, path)) {
-      return true
-    }
-    if (search(node.right, target, path)) {
-      return true
-    }
-    path.pop()
-    false
   }
 }
