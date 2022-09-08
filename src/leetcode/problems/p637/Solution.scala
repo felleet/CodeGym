e9df35af9cd11cb5bb65634ca6aceab8ea9ce186
felleet <felleet@gmail.com>
2022-09-08 11:21:50 +0300

    [+] 637. Average of Levels in Binary Tree

--- src/leetcode/problems/p637/Solution.scala
+++ src/leetcode/problems/p637/Solution.scala
@@ -7,17 +7,17 @@ object Solution {
     val map = new collection.mutable.HashMap[Int, (Int, Double)]()
     val level = 1
     map.put(level, (1, root.value))
-    dive(root.left, level + 1, map)
-    dive(root.right, level + 1, map)
+
+    def dive(node: TreeNode, level: Int): Unit = {
+      if (node == null) return
+      val (count, sum) = map.getOrElse(level, (0, 0d))
+      map.put(level, (count + 1, sum + node.value))
+      dive(node.left, level + 1)
+      dive(node.right, level + 1)
+    }
+
+    dive(root.left, level + 1)
+    dive(root.right, level + 1)
     map.values.map { case (count, sum) => sum / count }.toArray
   }
-
-  def dive(node: TreeNode, level: Int, map: collection.mutable.HashMap[Int, (Int, Double)]):Unit = {
-    if (node == null) return
-    val (count, sum) = map.getOrElse(level, (0,0d))
-    map.put(level, (count + 1, sum + node.value))
-    dive(node.left, level + 1, map)
-    dive(node.right, level + 1, map)
-  }
-
 }
