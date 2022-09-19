dae448c56b4bce809a3bff37e742ebc8cb96969d
felleet <felleet@gmail.com>
2022-09-19 20:24:13 +0300

    [+] 977. Squares of a Sorted Array v2 slower :)

--- src/leetcode/problems/p977/Solution.scala
+++ src/leetcode/problems/p977/Solution.scala
@@ -2,6 +2,20 @@ package leetcode.problems.p977
 
 object Solution {
   def sortedSquares(nums: Array[Int]): Array[Int] = {
-    nums.map(x => x * x).sorted
+    val arr: Array[Int] = new Array(nums.length)
+
+    var leftIndex = 0
+    var rightIndex = nums.length-1
+
+    (nums.length - 1 to 0 by -1).foreach { i =>
+      if (Math.abs(nums(leftIndex)) > Math.abs(nums(rightIndex))) {
+        arr(i) = nums(leftIndex) * nums(leftIndex)
+        leftIndex += 1
+      } else {
+        arr(i) = nums(rightIndex) * nums(rightIndex)
+        rightIndex -= 1
+      }
+    }
+    arr
   }
 }
